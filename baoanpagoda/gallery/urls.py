21a6f85a21b32d1293e20b4f76e8039cfccf325a
thang42c3 <ducthangbnn@gmail.com>
2023-10-09 22:27:56 +0700

    updated

--- baoanpagoda/gallery/urls.py
+++ baoanpagoda/gallery/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('list_category', views.list_categories, name='list_category'),
+    path('list_category/<slug:category_slug>/', views.list_categories, name='list_category'),
 ]
 
--- baoanpagoda/gallery/views.py
+++ baoanpagoda/gallery/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from . models import ImageGallery, MediaGallery, GalleryCategories
 from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 
-def list_categories(request):
-    image_gallery_list = ImageGallery.objects.all()
+def list_categories(request, category_slug=None):
+    category = get_object_or_404(GalleryCategories, en_slug=category_slug)
+    image_gallery_list = ImageGallery.objects.filter(category=category)
     paginator = Paginator(image_gallery_list, 9)
     page = request.GET.get('page')
     image_galleries = paginator.get_page(page)
@@ -14,4 +16,5 @@ def list_categories(request):
     gallery_categories = GalleryCategories.objects.all()
     return render(request, 'list-category.html', {'image_galleries': image_galleries,
                                                   'media_galleries': media_galleries,
-                                                  'gallery_categories': gallery_categories})
+                                                  'gallery_categories': gallery_categories,
+                                                  'category':category})
